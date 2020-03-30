bfc95f930d7513705d0dce18986502c2f5b6b3f2
Alexandre Ferreira <alexjuniorbr@gmail.com>
2020-03-30 10:38:21 +0900

    mobile finished

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,75 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+        marginTop: 36,
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        color:'#737380'
+    },
+
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+    },
+
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#13131a',
+        lineHeight: 30,
+    },
+
+    heroDescription: {
+        fontSize: 15,
+        color: '#737380',
+        marginTop: 8,
+    },
+    
+    actions: {
+        marginTop: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    action: {
+        backgroundColor: '#e02041',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    actionText: {
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+});
